78ec42321908e89f01c311acdc1c1fd7a4730c13
Wiktoria Bryniowska <w.bryniowska@gmail.com>
2022-05-09 00:00:49 +0200

    Add: genetation of tags in the sidebar

--- js/script.js
+++ js/script.js
@@ -36,6 +36,7 @@ const optArticleSelector = ".post",
   optTitleSelector = ".post-title",
   optTitleListSelector = ".titles",
   optArticleTagsSelector = ".post-tags .list",
+  optTagsListSelector = ".sidebar .tags",
   optArticleAuthorSelector= ".post .post-author";
 
 function generateTitleLinks(customSelector = "") {
@@ -84,6 +85,8 @@ function generateTitleLinks(customSelector = "") {
 
 
 function generateTags() {
+  /* [NEW] create a new variable allTags with an empty object */
+  let allTags = {};
   /* find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
   /* START LOOP: for every article: */
@@ -102,13 +105,36 @@ function generateTags() {
     for (let tag of articleTagsArray) {
       /* generate HTML of the link */
       const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + "</a></li>";
-
       /* add generated code to html variable */
       html = html + linkHTML;
+     /* [NEW] check if this link is NOT already in allTags */
+    if(!allTags[tag]) {
+      /* [NEW] add tag to allTags object */
+        allTags[tag] = 1;
+      }
+    else {
+        allTags[tag]++;
+      }
     } /* END LOOP: for each tag */
     /* insert HTML of all the links into the tags wrapper */
     tagWrappers.innerHTML = html;
   } /* END LOOP: for every article: */
+    /* [NEW] find list of tags in right column */
+    let tagList = document.querySelector(optTagsListSelector);
+    /* [NEW] create variable for all links HTML code */
+    let allTagsHTML = " ";
+
+    /* [NEW] START LOOP: for each tag in allTags: */
+    for(let tag in allTags){
+      /* [NEW] generate code of a link and add it to allTagsHTML */
+      allTagsHTML += '<li><a href="#tag-' + tag + '">' + tag + ' (' + allTags[tag] + ')</a></li>';
+    }
+    console.log(allTagsHTML);
+
+    /* [NEW] END LOOP: for each tag in allTags: */
+
+    /*[NEW] add HTML from allTagsHTML to tagList */
+    tagList.innerHTML = allTagsHTML;
 }
 
 
